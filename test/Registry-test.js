249a16e964ff061dee79c64bceb1de8a73251419
manikantanynala97 <manikantanynala97@gmail.com>
2022-05-29 17:09:41 +0530

    Manikanta Commit

---
+++ test/Registry-test.js
@@ -0,0 +1,11 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Registry = await ethers.getContractFactory("Registry");
+    const registry = await Registry.deploy("0xF9d5230B1AA44Dba47a96682D0CdC8Be5d38f34f");
+    await registry.deployed();
+
+  });
+});
